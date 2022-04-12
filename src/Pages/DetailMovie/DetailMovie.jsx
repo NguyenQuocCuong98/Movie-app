0f8b6fe23f10b1312e5fb1fa5bfc127e6b1116e3
Nguyễn Quốc Cường <anhyeunguyen1998@gmail.com>
2022-04-12 21:41:00 +0700

    new componets

--- src/Pages/DetailMovie/DetailMovie.jsx
+++ src/Pages/DetailMovie/DetailMovie.jsx
@@ -4,6 +4,7 @@ import {
   TMDB_IMAGE,
   TMDB_IMAGE_W500,
 } from "../../utils/constans";
+import { Col, Row } from "antd";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 
@@ -66,15 +67,76 @@ const DetailMovie = () => {
         </div>
       </div>
       <div className={classes.listMovie}>
-        <div className={classes.inforMation}>
-          <div className={classes.detailMovie}>
-            <img
-              className={classes.imageMovie}
-              src={`${TMDB_IMAGE_W500}${detailMovie.poster_path}`}
-              alt={detailMovie.name || detailMovie.title}
-            />
+        <Row gutter={30}>
+          <div className={classes.container}>
+            <Col sm={24} md={24} lg={24} xl={20}>
+              <div className={classes.wrapper}>
+                <div className={classes.detailMovie}>
+                  <img
+                    className={classes.imageMovie}
+                    src={`${TMDB_IMAGE_W500}${detailMovie.poster_path}`}
+                    alt={detailMovie.name || detailMovie.title}
+                  />
+                </div>
+                <div className={classes.information}>
+                  <h1>{detailMovie.name || detailMovie.title}</h1>
+                  <div className={classes.buy}>
+                    <button className={classes.vip}>buy vip package</button>
+                    <button className={classes.phone}>
+                      Buy quickly by phone
+                    </button>
+                  </div>
+                  <div className={classes.btnUser}>
+                    <button className={classes.heart}>
+                      <i className="fa-solid fa-heart"></i>
+                      Follow
+                    </button>
+                    <button className={classes.nodes}>
+                      <i className="fa-solid fa-share-nodes"></i>
+                      Share
+                    </button>
+                  </div>
+                  <div className={classes.description}>
+                    <h3>Overview</h3>
+                    <p>{detailMovie.overview}</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col xl={4}>
+              <div className={classes.author}>
+                <div className={classes.time}>
+                  <h4>Time</h4>
+                  <h4>Rating</h4>
+                  <h4>Director</h4>
+                  <h4>Actor</h4>
+                  <h4>Nation</h4>
+                  <h4>Type</h4>
+                  <h4>Release</h4>
+                </div>
+                <div className={classes.timeDes}>
+                  <span>
+                    {detailMovie.runtime}
+                    minute
+                  </span>
+                  <span>16+</span>
+                  <span>
+                    {detailMovie.production_companies.map(
+                      (production) => production.name
+                    )}
+                  </span>
+                  <span>
+                    {detailMovie.production_countries.map(
+                      (countrie) => countrie.name
+                    )}
+                  </span>
+                  <span>{detailMovie.genres.map((genre) => genre.name)}</span>
+                  <span>{detailMovie.release_date}</span>
+                </div>
+              </div>
+            </Col>
           </div>
-        </div>
+        </Row>
       </div>
     </section>
   );
--- src/components/Layout/NavMenu/MainMenu.jsx
+++ src/components/Layout/NavMenu/MainMenu.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import classes from "../../Layout/NavMenu/MainMenu.module.css";
 import logoHBO from "../../../assets/logohbo.jpg";
+
 const MainMenu = () => {
   return (
     <div className={classes["nav-menu"]}>
       <img src={logoHBO} alt="logo" />
       <div className={classes.peseudo} />
       <ul>
-        <li>Phim bộ </li>
-        <li>Điện ảnh</li>
-        <li>Châu Á</li>
-        <li>Tài liệu</li>
-        <li>Thiếu nhi & Gia đình</li>
+        <li>Movie</li>
+        <li>Film</li>
+        <li>Asia</li>
+        <li>Documents</li>
+        <li>Children & Families</li>
       </ul>
     </div>
   );
